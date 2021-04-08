6d52d70af108a9403a7d62339341490ac111bc9b
BrunoRicci <bruno.ricci.963@gmail.com>
2021-04-08 01:08:13 -0300

    handled files. Iterated files. Number and dots replacements/corrections OK. Pending: Uppercases , adding spaces at text line very end

--- Exercises/Lab 3/Lab_3/main.c
+++ Exercises/Lab 3/Lab_3/main.c
@@ -5,6 +5,7 @@
 #define LINE_MAX_CHARACTERS 25
 #define INPUT_FILE_NAME "input.txt"
 #define OUTPUT_FILE_NAME "output.txt"
+#define BUFFER_SIZE 1000  //1kB static buffer...
 
 
 int main()
@@ -12,8 +13,8 @@ int main()
     FILE *origin_file=0, *output_file=0;
     unsigned long int file_length=0;
     char c, to_store_value;
-    char line_string[LINE_MAX_CHARACTERS+1];
-    char *buffer;
+    char line_string[LINE_MAX_CHARACTERS+7]="", character_counter;
+    char buffer[BUFFER_SIZE];
 
 
     origin_file = fopen(INPUT_FILE_NAME,"r");   //Open file to read and write
@@ -24,46 +25,67 @@ int main()
         printf("Origin address: %x\n", &origin_file);
         printf("Origin content: %x\n", origin_file);
 
-        ///////////////////
-        fseek(origin_file, 0, 2);                   //Get cursor to the end of the file
-        file_length = ftell(origin_file);           //Cursor position.
-        fseek(origin_file, -file_length, 1);        //Return cursor to original value.
-
-        buffer = malloc((sizeof(char) * file_length)+1);    //Allocates RAM to store into array.
-        ////////////////////////
-
-        printf("Origin content: %x\n", origin_file);
-        printf("File length: %d\n", file_length);
 
         unsigned long int i=0;
         while((c = fgetc(origin_file)) != EOF ){
             buffer[i] = c; //Copies char value to buffer
-            printf("%d",i);
             i++;
         }
-        //buffer[i] = c;
-        printf("%d", i);
-        printf("\n buffer: %s \n",buffer);
+        file_length = i;    //Saves file size
+        printf("File length: %d\n", i);
+        //printf("\n\nbuffer: %s \n",buffer);
 
-        for(i=0; i < file_length ; i++){    //Iterate buffer
-            to_store_value = buffer[i];
-            printf("%c(%d) ",to_store_value, i);
+
+
+        character_counter=1;
+
+        for(i=0; i < file_length  ; i++)    //Iterate buffer
+        {
+
+            printf("%c",buffer[i], i);
+
+            if(buffer[i] >= '0' && buffer[i] <= '9')    //Filters, conditions, etc.
+            {
+                //printf("\nnumber\n");
+                strcat(line_string, "*");
+            }
+            else if((buffer[i] == '.' || buffer[i] == ',' || buffer[i] == ';' || buffer[i] == ':' || buffer[i] == '!' || buffer[i] == '?') && (buffer[i+1] != ' ' || buffer[i+1] != '\n') )
+            {
+                //printf("\ndot\n");
+                strcat(line_string, ". ");
+            }
+            else{
+                //printf("\ncharacter\n");
+                char x[2]={0,0};
+                x[0]=(char)buffer[i];
+                strcat(line_string, x);
+            }
+
+            if(character_counter >= LINE_MAX_CHARACTERS || buffer[i] == '\n' )
+            {
+                char x[3]={0,0,0};
+                //Add character count
+                line_string[character_counter]=' '; //Replace for SPACE symbol
+                strcat(line_string, "| c:");
+                itoa(character_counter, x , 10);  //Convert number to array
+                strcat(line_string, x);
+                strcat(line_string, "\n");
+
+                printf("\nline_string: %s\n", line_string);//debug
+                fputs(line_string, output_file);    //Save line to file
+
+                clearBuffer(line_string, sizeof(line_string));
+
+                character_counter = 0;
+            }
+            character_counter++;
+
+            //fputc(to_store_value,output_file);      //Write character.
         }
 
-//        while ((c = getc(origin_file)) != EOF)       //Iterate the whole file.
-//        {
-//            to_store_value = c;
-//            printf("%c(%d) ",to_store_value,ftell(origin_file));
-//
-//            if(c >= '0' && c <= '9')    //Filters, conditions, etc.
-//                to_store_value = '*';
-//            else if(c == '.' && 1)
-//            {
-//
-//            }
-//
-//            fputc(to_store_value,output_file);      //Write character.
-//        }
+        printf("\n\nline_string: %s", line_string);
+
+
 
 
 
@@ -73,7 +95,7 @@ int main()
     else
         printf("Could not open the file.");
 
-    //showOutputFile(output_file);
+    showOutputFile(output_file);
 
     fclose(origin_file);
     fclose(output_file);
@@ -86,4 +108,15 @@ void showOutputFile(FILE *f)
     char c;
     while ((c = getc(f)) != EOF)       //Iterate the whole file.
         printf("%c",c);
+}
+
+void clearBuffer(char *b, unsigned short int size)
+{
+    char i=0;
+    while(i<size)
+    {
+        *(b+i)=0;
+        i++;
+    }
+
 }
